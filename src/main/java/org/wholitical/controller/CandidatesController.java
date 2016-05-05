0da34169781632386dc6e11c89c227002416a505
Lee Dobryden <lndobryden@gmail.com>
2016-05-04 21:23:13 -0400

    Add find candidates by zipcode

--- src/main/java/org/wholitical/controller/CandidatesController.java
+++ src/main/java/org/wholitical/controller/CandidatesController.java
@@ -3,17 +3,17 @@ package org.wholitical.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.wholitical.domain.Candidates;
+import org.wholitical.domain.District;
 import org.wholitical.domain.States;
+import org.wholitical.repository.DistrictRepository;
 import org.wholitical.service.OpenFECService;
 import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.time.LocalDate;
 
 /**
  *
@@ -25,6 +25,9 @@ public class CandidatesController {
     @Autowired
     private OpenFECService openFECService;
 
+    @Autowired
+    private DistrictRepository districtRepository;
+
     @Value("${openfec.apiKey}")
     private String fecApiKey;
 
@@ -35,5 +38,13 @@ public class CandidatesController {
         return candidatesResponse.body();
     }
 
+    @RequestMapping(value= "/candidates/{zipcode}", method = RequestMethod.GET)
+    public Candidates byZipcode(@PathVariable int zipcode) throws IOException {
+        District district = districtRepository.findOne(zipcode);
+
+        Call<Candidates> toReturn = openFECService.getCandidatesByStateAndDistrict(States.parse(district.getState()).getANSIabbreviation(), district.getDistrict(), LocalDate.now().getYear(), fecApiKey);
+        Response<Candidates> candidatesResponse = toReturn.execute();
+        return candidatesResponse.body();
+    }
 
 }
