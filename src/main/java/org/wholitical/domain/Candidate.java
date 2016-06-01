468686905b8da35ea6ffb2a2ace5ec31347f4dd4
Lee Dobryden <lndobryden@gmail.com>
2016-05-31 21:14:39 -0400

    Ignore unknown properties for candidate

--- src/main/java/org/wholitical/domain/Candidate.java
+++ src/main/java/org/wholitical/domain/Candidate.java
@@ -1,11 +1,13 @@
 package org.wholitical.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 /**
  *
  */
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Candidate {
 
     private String office;
